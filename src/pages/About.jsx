3583719efaf9b7d903f46fd578d14fa1c1c860ff
MarianaDam420 <utm23090799@utma.edu.mx>
2023-11-07 19:37:32 -0600

    Added react routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export function About () {
+    return(
+        <div>
+            <h1 className="text-8xl text-indigo-500">Hello From About</h1>
+        <Link to="/contact>">Contact</Link>
+        </div>
+    )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export function Contact() {
+return (
+    <div>
+        <h1 className="text-8xl text-indigo-500">Hello From Contact</h1>
+        <Link to="/">Home</Link>
+    </div>
+)
+
+}
